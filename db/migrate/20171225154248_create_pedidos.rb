fe2fab1a6d6c6fb52a80a043f6d46dd2623cf1f4
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2017-12-25 12:48:29 -0300

    Navidad

---
+++ db/migrate/20171225154248_create_pedidos.rb
@@ -0,0 +1,24 @@
+class CreatePedidos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :pedidos do |t|
+      t.string :nombre
+      t.boolean :carne
+      t.boolean :pollo
+      t.boolean :falafel
+      t.boolean :mayonesa
+      t.boolean :salsa
+      t.boolean :humus
+      t.boolean :tahini
+      t.boolean :yogurt
+      t.boolean :lechuga
+      t.boolean :tomate
+      t.boolean :cebolla
+      t.boolean :repollo
+      t.boolean :zanahoria
+      t.boolean :ciboulette
+      t.boolean :aceitunas
+      t.boolean :picante
+      t.boolean :papaspay
+    end
+  end
+end
